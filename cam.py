2acd5e27d71a791a7ced8e0e5dc846cf89ce01e4
Arty <arty@arty.com>
2025-11-25 02:59:42 -0500

    PC->RPI

--- cam.py
+++ cam.py
@@ -17,7 +17,7 @@ GPIO_RIGHT = 13
 
 #pwm.setPWMFreq(50)
 
-camera = PiCamera()
+camera = Picamera2()
 camera.resolution = (640, 480)
 camera.framerate = 60
 rawCapture = PiRGBArray(camera, size=(640, 480))
