610fb2e7ef655a5e66976d7026b1466b8251f7ff
Arty <arty@arty.com>
2025-11-27 23:58:03 -0500

    PC->RPI

--- cam.py
+++ cam.py
@@ -8,7 +8,7 @@ import picamera2
 from picamera2 import Picamera2
 
 import RPi.GPIO as GPIO
-from threading import Thread
+from threading import Thread, Lock, Condition
 
 GPIO_LEFT = 16
 GPIO_RIGHT = 26
@@ -24,7 +24,7 @@ camera.start()
 # allow the camera to warmup
 time.sleep(1)
 
-faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_LEFT, GPIO.OUT)
@@ -37,110 +37,103 @@ t = time.time()
 deltaTime = 0
 averageTime = 0
 
-global frameBuffer
-global frameBufferHasData
-global backupFrameBuffer
-global backupFrameBufferHasData
-global mainReadingFrameBuffer
-global mainReadingBackupFrameBuffer
-global workerWritingFrameBuffer
-global workerWritingBackupFrameBuffer
-
 frameBuffer = []
 frameBufferHasData = False
 backupFrameBuffer = []
 backupFrameBufferHasData = False
-mainReadingFrameBuffer = False
-mainReadingBackupFrameBuffer = False
-workerWritingFrameBuffer = False
-workerWritingBackupFrameBuffer = False
+frameBufferLock = Lock()
+backupFrameBufferLock = Lock()
 
 def BackgroundWork():
     global frameBuffer
-    global frameBufferHasData
     global backupFrameBuffer
+    global frameBufferLock
+    global backupFrameBufferLock
+    global frameBufferHasData
     global backupFrameBufferHasData
-    global mainReadingFrameBuffer
-    global mainReadingBackupFrameBuffer
-    global workerWritingFrameBuffer
-    global workerWritingBackupFrameBuffer
     
     # Read the next frame from the stream in a different thread
-    print("Starting Worker Thread")
+    print("Starting Camera Worker Thread")
     while True:
-        if mainReadingFrameBuffer or frameBufferHasData:
-            workerWritingBackupFrameBuffer = True
-            backupFrameBuffer = camera.capture_array()
-            workerWritingBackupFrameBuffer = False
-            backupFrameBufferHasData = True
-        else:
-            workerWritingFrameBuffer = True
+        if frameBufferLock.acquire(False):
             frameBuffer = camera.capture_array()
-            workerWritingFrameBuffer = False
+            frameBufferLock.release()
             frameBufferHasData = True
-            
-        time.sleep(.001)
+        elif backupFrameBufferLock.acquire(False):
+            backupFrameBuffer = camera.capture_array()
+            backupFrameBufferLock.release()
+            backupFrameBufferHasData = True
+
+
+grayscaleFramebuffer = []
+grayscaleFrameLock = Lock()
+faces = []
+facesLock = Lock()
+faceDataAvailable = False
+
+def MLWorker():
+    global facesLock
+    global faces
+    global grayscaleFramebuffer
+    global faceDataAvailable
+    
+    print("Started ML Worker Thread")
+    while True:
+        if faceDataAvailable == False:
+            with facesLock.acquire():
+                with grayscaleFrameLock.acquire():
+                    faces = faceCascade.detectMultiScale(
+                        grayscaleFramebuffer,
+                        scaleFactor=1.2,
+                        minNeighbors=5,
+                        minSize=(20, 20)
+                    )
+                    faceDataAvailable = True
+
 
 thread = Thread(target=BackgroundWork, args=())
 thread.daemon = True
 thread.start()
 
-faces = []
-faceDataAvailable = False
-
-def MLWorker(*gray):
-    global faceDataAvailable
-    global faces
-    faces = faceCascade.detectMultiScale(
-        gray[0],
-        scaleFactor=1.2,
-        minNeighbors=5,
-        minSize=(20, 20)
-    )
-    faceDataAvailable = True
+thread = Thread(target=MLWorker, args=())
+thread.daemon = True
+thread.start()
 
 print("Starting Main Thread")
 while True:
     if faceDataAvailable == True:
-        newTime = time.time()
-        deltaTime = newTime - t
-        averageTime = (averageTime + deltaTime)/2
-        t = newTime
-        print("FPS=" + str(1/deltaTime) + " AVG=" + str(1/averageTime))
+        with facesLock.acquire():
+            newTime = time.time()
+            deltaTime = newTime - t
+            averageTime = (averageTime + deltaTime)/2
+            t = newTime
+            print("FPS=" + str(1/deltaTime) + " AVG=" + str(1/averageTime))
 
-        faceDataAvailable = False
-        for (x, y, w, h) in faces:
-            mainReadingFrameBuffer = True
-            cv2.rectangle(frameBuffer, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            mainReadingFrameBuffer = False
+            for (x, y, w, h) in faces:
+                
+                with grayscaleFrameLock.acquire(False):
+                    cv2.rectangle(grayscaleFramebuffer, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                
+                if x > 240:
+                    print("Left")
+                    GPIO.output(GPIO_LEFT, GPIO.HIGH)
+                    GPIO.output(GPIO_RIGHT, GPIO.LOW)
+                elif x < 220:
+                    print("Right")
+                    GPIO.output(GPIO_RIGHT,GPIO.HIGH)
+                    GPIO.output(GPIO_LEFT,GPIO.LOW)
+                else:
+                    print("Center Or No face")
+                    GPIO.output(GPIO_RIGHT,GPIO.LOW)
+                    GPIO.output(GPIO_LEFT,GPIO.LOW)
 
-            
-            if x > 240:
-                print("Left")
-                GPIO.output(GPIO_LEFT, GPIO.HIGH)
-                GPIO.output(GPIO_RIGHT, GPIO.LOW)
-            elif x < 220:
-                print("Right")
-                GPIO.output(GPIO_RIGHT,GPIO.HIGH)
-                GPIO.output(GPIO_LEFT,GPIO.LOW)
-            else:
-                print("Center Or No face")
-                GPIO.output(GPIO_RIGHT,GPIO.LOW)
-                GPIO.output(GPIO_LEFT,GPIO.LOW)
+            faceDataAvailable = False
 
-    if frameBufferHasData == False and backupFrameBufferHasData == False:
-        continue
-
-    gray = []
-    if frameBufferHasData and workerWritingFrameBuffer == False:
-        mainReadingFrameBuffer = True
-        gray = cv2.cvtColor(frameBuffer, cv2.COLOR_BGR2GRAY)
-        mainReadingFrameBuffer = False
+    if frameBufferHasData and frameBufferLock.acquire(False):
+        grayscaleFramebuffer = cv2.cvtColor(frameBuffer, cv2.COLOR_BGR2GRAY)
+        frameBufferLock.release()
         frameBufferHasData = False
-    elif backupFrameBufferHasData and workerWritingBackupFrameBuffer == False:
-        mainReadingBackupFrameBuffer = True
-        gray = cv2.cvtColor(backupFrameBuffer, cv2.COLOR_BGR2GRAY)
-        mainReadingBackupFrameBuffer = False
+    elif backupFrameBufferHasData and backupFrameBufferLock.acquire(False):
+        grayscaleFramebuffer = cv2.cvtColor(backupFrameBuffer, cv2.COLOR_BGR2GRAY)
+        backupFrameBufferLock.release()
         backupFrameBufferHasData = False
-    
-    Thread(target=MLWorker, args=(gray,)).run()
