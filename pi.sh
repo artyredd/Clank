0e6dd08530cf42d30c5939096a6fd9e59bd0c648
Arty <arty@arty.com>
2025-12-01 18:57:55 -0500

    PC->Pi

---
+++ pi.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt -y full-upgrade
+sudo apt -y install firefox
+sudo apt -y install git
+sudo apt -y install gh
+sudo apt -y install ripgrep
+sudo apt -y install pip
+sudo apt -y install pipx
+echo "GitHub Token: is in drive/backup/passwords/github clank key"
+sudo gh auth login
+sudo git clone https://github.com/artyredd/Clank.git
+echo enable pcie3, and auto-login
+sudo raspi-config
+echo did u enable pcie3, and auto-login?
+sudo reboot
+
+# reboot
+sudo apt update && sudo apt full-upgrade
+sudo rpi-eeprom-update
+sudo rpi-eeprom-update -a
+sudo reboot
+
+# reboot
+sudo apt -y install dkms
+sudo apt -y install hailo-all
+sudo reboot
+
+# reboot
+sudo apt update && sudo apt install rpicam-apps
+git clone https://github.com/hailo-ai/hailo-rpi5-examples.git
+cd hailo-rpi5-examples
+git checkout dev
+git pull
+# change version number to main on second line
+echo "change version number to main on second line"
+sudo nano /config/config.yaml
+sudo ./install.sh
+
+cd ~
+cp -r /Clank /hailo-rpi5-examples
--- scripts/cleanup_installation.sh
+++
@@ -1,2 +0,0 @@
- sudo rm -rf resources hailo_apps.egg-info/ venv_hailo_apps/ .env hailort.log 
- sudo rm -rf /usr/local/hailo/resources/
