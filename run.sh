47e977c789d34f43009e85a29bd02f283fbff298
Arty <arty@arty.com>
2025-12-01 19:17:25 -0500

    PC->RPI

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Starting"
-. ./home/arty/hailo-rpi5-examples/setup_env.sh
+. /home/arty/hailo-rpi5-examples/setup_env.sh
 python3 /home/arty/hailo-rpi5-examples/basic_pipelines/pose_estimation.py --input rpi
