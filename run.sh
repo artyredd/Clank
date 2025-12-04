f042b1d4eaecbe5f30ca49e70151d8b11dbc6be2
Arty <arty@arty.com>
2025-12-04 00:42:27 -0500

    PC->RPI

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 echo "Starting"
 cd /home/arty/hailo-rpi5-examples
 . setup_env.sh
-python3 /home/arty/hailo-rpi5-examples/basic_pipelines/pose_estimation.py --input rpi --show-fps --frame-rate 90
+python3 /home/arty/hailo-rpi5-examples/basic_pipelines/pose_estimation.py --input rpi --show-fps
