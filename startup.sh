e194ed665687a6462e1119b2293f44dfed0343c0
Arty <arty@arty.com>
2025-12-03 23:35:49 -0500

    PC->RPI

--- startup.sh
+++ startup.sh
@@ -12,5 +12,5 @@ sleep 1
 echo "[.....]"
 sleep 1
 
-cp /home/arty/Clank/basic_pipelines/pose_estimation.py /home/arty/hailo-examples/basic_pipelines/
+cp /home/arty/Clank/basic_pipelines/pose_estimation.py /home/arty/hailo-rpi5-examples/basic_pipelines/
 DISPLAY=:0 /home/arty/Clank/run.sh
