c3db846cd85d720ea47791722abbb927fed37586
Arty <arty@arty.com>
2025-12-12 01:33:52 -0500

    PC->RPI

--- basic_pipelines/pose_estimation.py
+++ basic_pipelines/pose_estimation.py
@@ -17,8 +17,8 @@ import RPi.GPIO as GPIO
 from threading import Thread, Lock, Condition
 import time
 
-PIN_LEFT = 19
-PIN_RIGHT = 13
+PIN_LEFT = 13
+PIN_RIGHT = 19
 PWM_FREQ = 200
 MAX_PWM_DUTY = 30
 DETECTION_MARGIN = 10
